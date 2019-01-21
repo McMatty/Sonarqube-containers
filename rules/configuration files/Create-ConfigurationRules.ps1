c25bf40e0745ce8ab57ff177618350bb582810aa
Matt Evans <Matt.Evans@iag.co.nz>
2019-01-22 12:28:38 +1300

    Completed script that loads custom xml rules

--- rules/configuration files/Create-ConfigurationRules.ps1
+++ rules/configuration files/Create-ConfigurationRules.ps1
@@ -2,8 +2,8 @@
 $rulesUri = "https://azsdkossep.azureedge.net/secintelrules.json"
 $rulesPath = "Universal.json"
 $sonarqube_host = "http://localhost:8001"
-$apiRulesPath = "/api/rules/create"
-$apiTokenPath = "/api/user_tokens/generate"
+$apiRulesUpdate = "/api/rules/update"
+$apiRulesCreate = "/api/rules/create"
 $credentials = "admin:admin" #default credentials - not dont hardcode these or use them in production
 
 $bytes = [System.Text.Encoding]::UTF8.GetBytes($credentials)
@@ -18,6 +18,7 @@ catch {
 
 if ([int]$response.StatusCode -eq 200) {
     #Process new ruleset - currently uri returns 404
+    Write-Host "Shit they've opened up the rules URI again!"
 }
 else {
     $rules = Get-Content -Path (Join-Path $PSScriptRoot $rulesPath) | ConvertFrom-Json
@@ -27,39 +28,48 @@ else {
         $issue = $descriptionSections[0] -replace "Issue:", ""
         $description = $descriptionSections[1] -replace "Description:", ""
         $description_expanded = $description + [Environment]::NewLine + [Environment]::NewLine + $descriptionSections[2]
-        $severity = "CRITICAL"
+        $severity = "MAJOR"
         $expression = $_.xPath
+        $failedCreating = $false
+        $firstWebException = ""
 
         $form = @{name           = $issue;
             markdown_description = $description_expanded;
             params               = "filePattern=""*.config"";expression=""$expression"";message=""$description""";
-            custom_key           = $_.id;
-            key                  = $_.id;
+            custom_key           = $_.id;         
+            key                  = "xml:" + $_.id;       
             type                 = "VULNERABILITY";
             template_key         = "xml:XPathCheck";
             severity             = $severity;
             prevent_reactivation = "true";
             status               = "READY"
-        }
+        } 
 
         try { 
-            Invoke-WebRequest -Uri $sonarqube_host$apiRulesPath `
+            Invoke-RestMethod -Uri $sonarqube_host$apiRulesUpdate `
                 -Method Post `
                 -Headers @{Authorization = "Basic $encodedText"} `
-                -Form $form
+                -Form $form   
         }
         catch {
-            $_.Exception.Message
+            $failedCreating = $true      
+            $firstWebException = $_.Exception.Message     
+        }  
+
+        #If we failed creating assume they exist - cannot use search to validate as search will not display deleted rules which retain their key and are soft-deleted
+        if ($failedCreating) {
+            try { 
+                Invoke-RestMethod -Uri $sonarqube_host$apiRulesCreate `
+                    -Method Post `
+                    -Headers @{Authorization = "Basic $encodedText"} `
+                    -Form $form
+            }
+            catch {
+                Write-Host "Rules not applied - exception messages from both requested supplied" -ForegroundColor Red
+                $_.Exception.Message
+                $firstWebException
+            } 
         }
+        
     }
 }
-
-function Get-Token {
-    #/api/user_tokens/generate -d "name=access_dotnet" --noproxy
-
-    Invoke-WebRequest -Uri $sonarqube_host$apiTokenPath `
-        -Method Post `
-        -Headers @{Authorization = "Basic $encodedText"} `
-        -Form @{name = "create_xml_custom"}
-}
-
